10de648a6d94a7f30fce700fd3e232eaf7bd3769
wolfayet0855 <online_alt02@yahoo.com>
2024-09-27 13:10:28 -0400

    Update case and assest catalog

--- Rhythm.swift
+++ Rhythm.swift
@@ -5,4 +5,16 @@
 //  Created by user on 9/27/24.
 //
 
-import Foundation
+import SwiftUI
+
+@main
+struct AppEntry: App {
+    @StateObject var myEvents = EventStore(preview: true)
+
+    var body: some Scene {
+        WindowGroup {
+            StartTabView()
+                .environmentObject(myEvents)
+        }
+    }
+}
