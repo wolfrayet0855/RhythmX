d16bc4d0f7c74dc612ccc00263c5a38b00054870
wolfayet0855 <online_alt02@yahoo.com>
2024-09-27 11:07:14 -0400

    Initial Commit

---
+++ Lunar/LunarApp.swift
@@ -0,0 +1,17 @@
+//
+//  LunarApp.swift
+//  Lunar
+//
+//  Created by user on 9/27/24.
+//
+
+import SwiftUI
+
+@main
+struct LunarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
